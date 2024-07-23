6090c4fc0e946c2e84dec3f721a1a3d28187eb26
Arda Burak Kontaş <burakkontass@gmail.com>
2024-07-23 12:48:00 +0300

    # Solved https://www.hackerrank.com/challenges/encryption

---
+++ Encryption/Encryption/Program.cs
@@ -0,0 +1,51 @@
+﻿var result = Result.encryption("iffactsdontfittotheorychangethefacts");
+
+var expected = "isieae fdtonf fotrga anoyec cttctt tfhhhs";
+
+Console.WriteLine($"Result: {result}");
+Console.WriteLine($"Expected: {expected}");
+Console.WriteLine($"Is expected value equals to result: {expected == result}");
+
+class Result
+{
+
+    /*
+     * Complete the 'encryption' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string encryption(string s)
+    {
+        s = s.Replace(" ", "");
+        var sLength = s.Length;
+        var rowCount = (int)Math.Sqrt(sLength);
+        int columnCount;
+
+        if (rowCount * rowCount == sLength) columnCount = rowCount;
+        else if (rowCount * (rowCount + 1) >= sLength) columnCount = rowCount + 1;
+        else
+        {
+            rowCount++;
+            columnCount = rowCount;
+        }
+
+        var newString = "";
+        for (var i = 0; i < columnCount; i++)
+        {
+            for (var j = 0; j < rowCount; j++)
+            {
+                if (i + j * columnCount < sLength) newString += s[i + j * columnCount];
+                else break;
+                
+            }
+
+            newString += " ";
+        }
+
+        return newString.TrimEnd();
+    }
+
+
+}
