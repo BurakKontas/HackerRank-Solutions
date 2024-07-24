a72c90feb73a34d36db1ed5e0901a1febf20e95b
Arda Burak Kontaş <burakkontass@gmail.com>
2024-07-24 08:31:43 +0300

    # Solved https://www.hackerrank.com/challenges/extra-long-factorials

---
+++ Extra Long Factorials/Extra Long Factorials/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+Result.extraLongFactorials(25);
+
+// expected: 15511210043330985984000000
+
+class Result
+{
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n)
+    {
+        BigInteger result = 1;
+        while (n > 0)
+        {
+            result *= n--;
+        }
+
+        Console.WriteLine(result);
+    }
+
+}
